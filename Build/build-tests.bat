0c62050cb7c2cb9962a1cacf203c92f7e40ce798
Andreas Gullberg Larsen <anjdreas@gmail.com>
2017-05-27 13:12:09 +0200

    Update build scripts to use MSBuild Tools 2017 (VS2017 Community)

--- Build/build-tests.bat
+++ Build/build-tests.bat
@@ -1,4 +1,4 @@
 @echo off
 SET ROOT=%~dp0..
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" %ROOT%\Build\tests.msbuild /verbosity:normal /p:Configuration=Release /p:Platform="AnyCPU" /target:CleanAndBuild /p:RestorePackages=false
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" %ROOT%\Build\tests.msbuild /verbosity:normal /p:Configuration=Release /p:Platform="AnyCPU" /target:CleanAndBuild /p:RestorePackages=false
 if %errorlevel% neq 0 exit /b %errorlevel%
